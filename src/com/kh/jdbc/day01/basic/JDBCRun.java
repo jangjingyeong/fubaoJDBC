d6a7d4dc53b00c9d3f14c0a7fe8c44af49b6038d
jangjingyeong <jjg070593@gmail.com>
2023-07-20 17:17:11 +0900

    [INIT] 23/07/20 JDBC 프로그래밍 개념 및 실습
    1. JDBC 드라이버, ojdbc6.jar ADD 중요
      - Class.forName()
    2. JDBC 코딩절차에 따른 Class, Interface 사용
      - DriverManeger, Connection, Statement, ResultSet(SELECT)
    3. 후처리의 중요성
      - next(), getString(), getInt(), getDate(), ...
    4. 학생관리 프로그램 MVC 패턴 적용 후 JDBC 프로그래밍 완료(학생 전체 정보 조회)

---
+++ src/com/kh/jdbc/day01/basic/JDBCRun.java
@@ -0,0 +1,50 @@
+package com.kh.jdbc.day01.basic;
+
+import java.sql.*; // sql에 있는 거 다 쓰겠다고 1번만 import
+
+public class JDBCRun {
+	
+	public static void main(String[] args) {
+		/*
+		 *  JDBC 코딩 절차
+		 *  1. 드라이버 등록 (ojdbc6.jar파일이랑 관련) 
+		 *  2. DBMS 연결 생성
+		 *  3. Statement 객체 생성(쿼리문 실행 준비)
+		 *   - new Statement();가 아니라 연결을 통해 객체 생성함. 
+		 *  4. SQL 전송(쿼리문 실행)
+		 *  5. 결과 받기(ResultSet으로 바로 받아버림)
+		 *  6. 자원해제(close())
+		 *  
+		 */
+		
+		try {
+			String url = "jdbc:oracle:thin:@localhost:1521:xe"; // @ip:port:dbname
+			String user = "KH";
+			String password = "KH";
+			String query = "SELECT EMP_NAME, SALARY FROM EMPLOYEE";
+			// 1. 드라이버 등록
+			Class.forName("oracle.jdbc.driver.OracleDriver"); // 이 코드가 있음으로써 jar파일을 쓸 수 있게 되는 것임 
+			// 2. DBMS 연결 생성 
+			Connection conn = 
+					DriverManager.getConnection(url , user, password); // 이 연결도 checkedException
+			// 3. 쿼리문 실행 준비(Statement 객체 생성) : 연결을 통해 객체 생성 
+			Statement stmt = conn.createStatement();
+			// 4. 쿼리문 실행(SELECT면 ResultSet), 5. 결과값 받기(resultset은 테이블형태) 
+			ResultSet rset = stmt.executeQuery(query);
+			// 후처리 필요 - 디비에서 가져온 데이터를 사용하기 위함.
+			while(rset.next()) {
+				System.out.printf("직원명 : %s, 급여 : %s\n"
+						, rset.getString("EMP_NAME"), rset.getInt(2));
+			}
+			// 6. 자원해제
+			rset.close();
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+}
